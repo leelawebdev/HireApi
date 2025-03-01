d610415a0779c3dd72eb09269e2683e55556df9d
LeelaNarasimha <leelanarasimha@teamvelocitymarketing.com>
2025-03-01 09:41:39 +0530

    Implement user signup with password hashing and role assignment

--- src/features/user/controllers/auth.controller.ts
+++ src/features/user/controllers/auth.controller.ts
@@ -1,10 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import authService from '../services/auth.service';
 
 class AuthController {
   async signUp(req: Request, res: Response, next: NextFunction) {
-    res.status(StatusCodes.OK).json({
-      message: 'signup ',
+    const user = await authService.signup(req.body);
+
+    res.status(StatusCodes.CREATED).json({
+      message: 'User signup successfull',
+      data: user,
     });
   }
 
--- src/features/user/services/auth.service.ts
+++ src/features/user/services/auth.service.ts
@@ -1,3 +1,22 @@
-class AuthService {}
+import prisma from '../../../globals/prisma';
+import bcrypt from 'bcrypt';
+
+class AuthService {
+  async signup(requestBody: any) {
+    const { name, email, password } = requestBody;
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashPassword,
+        role: 'CANDIDATE',
+      },
+    });
+    return user;
+  }
+}
 
 export default new AuthService();
